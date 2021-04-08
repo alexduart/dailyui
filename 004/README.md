103d3620bd7701b3142d94130f12b4af1a52ad44
Alex Duarte <alexduart@gmail.com>
2021-04-08 09:48:37 -0300

    Daily UI

---
+++ 004/README.md
@@ -0,0 +1,6 @@
+# Daily UI #004
+
+Design a calculator. Standard, scientific, or specialty calculator for something such as a mortgage? Is it for a phone, a tablet, a web app?  #DailyUI #004
+
+
+![alt text](https://pbs.twimg.com/media/Eyc-Fr5W8AEocJT?format=jpg&name=large)
---
+++ 004/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DailyUI #004</title>
+	<link rel="stylesheet" href="css/base.css?<?php echo rand(); ?>">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="card-size card">
+			<div class="grid-container-result">
+				<div class="grid-item">
+					<div class="result">
+						<span class="equation">25 + 25 =</span>
+						<span class="aftermath">50</span>
+					</div>
+				</div>
+			</div>
+			<div class="grid-container-functions">
+				<div class="functions grid-item"><span>MC</span></div>
+				<div class="functions grid-item"><span>MR</span></div>
+				<div class="functions grid-item"><span>M+</span></div>
+				<div class="functions grid-item"><span>M-</span></div>
+				<div class="functions grid-item"><span>MS</span></div>
+				<div class="functions grid-item"><span>M <i class="fas fa-sort-down"></i></span></div>
+			</div>
+			<div class="grid-container-functions2">
+				<div class="command grid-item"><span>ce</span></div>
+				<div class="command grid-item"><span><i class="fas fa-percentage"></i></span></div>
+				<div class="command grid-item"><span><i class="fas fa-backspace"></i></span></div>
+			</div>
+			<div class="grid-container-command">
+				<div class="command grid-item"><span>1/x</span></div>
+				<div class="command grid-item"><span><i class="fas fa-superscript"></i></span></div>
+				<div class="command grid-item"><span><i class="fas fa-square-root-alt"></i></span></div>
+				<div class="command grid-item"><span><i class="fas fa-divide"></i></span></div>
+
+				<div class="command grid-item numbers"><span>7</span></div>
+				<div class="command grid-item numbers"><span>8</span></div>
+				<div class="command grid-item numbers"><span>9</div>
+				<div class="command grid-item"><span><i class="fas fa-times"></i></span></div>
+
+				<div class="command grid-item numbers"><span>4</span></div>
+				<div class="command grid-item numbers"><span>5</span></div>
+				<div class="command grid-item numbers"><span>6</div>
+				<div class="command grid-item"><span><i class="fas fa-minus"></i></span></div>
+
+				<div class="command grid-item numbers"><span>1</span></div>
+				<div class="command grid-item numbers"><span>2</span></div>
+				<div class="command grid-item numbers"><span>3</div>
+				<div class="command grid-item plus"><span><i class="fas fa-plus"></i></span></div>
+
+				<div class="command grid-item"><span>c</span></div>
+				<div class="command grid-item numbers"><span>0</span></div>
+				<div class="command grid-item"><span>,</div>
+				<div class="command grid-item"><span><i class="fas fa-equals"></i></span></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
